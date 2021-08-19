41bcb0237a406d6c720372965a47886742676fdb
kilogold <bonilla_kelvin@hotmail.com>
2021-08-19 16:07:02 -0700

    Testing flow setup

--- scripts/Deploy.py
+++ scripts/Deploy.py
@@ -2,5 +2,5 @@ from brownie import accounts, Ticket, TicketShop
 accounts.load("Metamask")
 
 def main():
-    token_tix = Ticket.deploy({'from': accounts[0]})
-    tixShop = TicketShop.deploy(token_tix, {'from': accounts[0]})
+    token_tix = Ticket.deploy({'from': accounts[0]},publish_source=True)
+    tixShop = TicketShop.deploy(token_tix, {'from': accounts[0]},publish_source=True)
